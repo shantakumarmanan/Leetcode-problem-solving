22692c082de9b4ca05e7992aa9c28be09c0c93e8
shanta <151567485+shantakumarmanan@users.noreply.github.com>
2026-07-20 15:36:48 +0530

    Time: 47 ms (34.94%) | Memory: 49.4 MB (25.24%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
+    int f(vector<int>& c, int i, int a, vector<vector<int>>& dp) {
+        if (i == 0) return (a % c[0] == 0) ? a / c[0] : 1e9;
+        if (dp[i][a] != -1) return dp[i][a];
+        int nt = f(c, i - 1, a, dp);
+        int t = 1e9;
+        if (c[i] <= a) t = 1 + f(c, i, a - c[i], dp);
+        return dp[i][a] = min(t, nt);
+    }
+
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, 1e9);
-        dp[0] = 0;
-        for (int c : coins)
-            for (int i = c; i <= amount; i++)
-                dp[i] = min(dp[i], dp[i - c] + 1);
-        return dp[amount] == 1e9 ? -1 : dp[amount];
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int ans = f(coins, n - 1, amount, dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
