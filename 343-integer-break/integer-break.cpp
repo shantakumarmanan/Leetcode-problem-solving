46a585532a7f1400a6b9d0d727bb7fac0eeb267b
shanta <151567485+shantakumarmanan@users.noreply.github.com>
2026-07-23 16:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (22.02%) - LeetSync

---
+++ 343-integer-break/integer-break.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& dp) {
+        if (n == 1) return 1;
+        if (dp[n] != -1) return dp[n];
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            ans = max(ans, max(i * (n - i), i * solve(n - i, dp)));
+        }
+        return dp[n] = ans;
+    }
+    int integerBreak(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+};
