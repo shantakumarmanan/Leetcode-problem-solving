ec4ab183cffe1fac1f648675394bd4e486a5fcf7
shanta <151567485+shantakumarmanan@users.noreply.github.com>
2026-08-17 00:08:49 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (81.51%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size() - 1;
+        int m = 0;
+        while(m<n){
+            swap(s[m], s[n]);
+            m++;
+            n--;
+        }
+    }
+};
