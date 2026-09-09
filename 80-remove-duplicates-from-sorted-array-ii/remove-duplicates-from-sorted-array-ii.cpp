22cb80744c3151698aa5de0ffd19c5d9ca07e489
shanta <151567485+shantakumarmanan@users.noreply.github.com>
2026-09-09 23:50:55 +0530

    Time: 13 ms (17.64%) | Memory: 21.1 MB (5.03%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_map<int, int> count;
+        int k = 0;
+        for (int num : nums) {
+            count[num]++;
+            if (count[num] <= 2) {
+                nums[k] = num;
+                k++;
+            }
+        }
+        return k;      
+    }
+};
